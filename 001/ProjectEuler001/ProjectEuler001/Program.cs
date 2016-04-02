3779924b25684a78109de56a516291731b385976
blemke <blemke@gmail.com>
2016-04-02 12:01:14 -0700

    working solution for 001

---
+++ 001/ProjectEuler001/ProjectEuler001/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int calcSum = Sum3and5s(1000);
+            Console.Write(calcSum);
+            Console.ReadKey();
+        }
+
+        static int Sum3and5s(int maxVal)
+        {
+            int sum = 0;
+            for(int i = 1; i < maxVal; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0)) {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+}
